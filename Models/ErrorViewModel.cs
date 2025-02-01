daa547e9310ceb03e9d74b13ce034536cdf1bd29
User <User@DESKTOP-HPHPFNO>
2025-02-01 13:37:22 +0700

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestEducationPortal4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
